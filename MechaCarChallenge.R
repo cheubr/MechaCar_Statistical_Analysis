83ba7310c91190588550e6f95c84c42df60cc426
Brandon Cheung <cheung.brandoncheung@gmail.com>
2022-04-10 22:16:40 -0400

    add technical deliverables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,15 +1,34 @@
 library(dplyr)
 
+## Linear Regression to Predict MPG
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names = F)
 
-lm (mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,data=MechaCar_mpg)
+lm (mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + 
+      vehicle_length,data=MechaCar_mpg)
 
-summary(lm (mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,data=MechaCar_mpg))
+summary(lm (mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + 
+              vehicle_length,data=MechaCar_mpg))
 
-
-
+## Summary Statistic on Suspension Coils
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names = F)
-total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variace=var(PSI), SD=sd(PSI), .groups = 'keep')
 
+
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variace=var(PSI), SD=sd(PSI), .groups = 'keep')
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variace=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+
+## T-Tests on Suspension Coils
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),
+       mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),
+       mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),
+       mu=mean(Suspension_Coil$PSI))
+
+## Study Design: MechaCar vs Competition
+
 
